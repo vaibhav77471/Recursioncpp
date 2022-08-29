584285f353e93c37464cf7a0b0c51d2cb95490b3
Vaibhav soni <100358265+vaibhav77471@users.noreply.github.com>
2022-08-29 21:56:49 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,22 @@
+include<iostream>
+using namespace std;
+
+// program of print factorial of nth term
+
+int factorial(int n){
+    //base case
+    if(n==0)
+    return 1;
+    
+  //Recursive call
+    return n * factorial(n-1);
+}
+int main(){
+    int n;
+    cout<<" enter the value of n to find factorial : ";
+    cin>>n;
+
+    int ans= factorial(n);
+    cout<<ans;
+    return 0;
+}
